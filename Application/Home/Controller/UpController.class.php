afb7c0577507c3686390559d99b3ece336e6e403
walle2eve <shmily_dhy@qq.com>
2015-10-12 23:43:07 +0800

    增加上传文件插件 bootstrap-fileinput

--- Application/Home/Controller/UpController.class.php
+++ Application/Home/Controller/UpController.class.php
@@ -17,6 +17,18 @@ class UpController extends PublicController {
 
 	//上传体质信息
 	public function phydata(){
+		if(IS_AJAX && $_FILES['file_data']){
+			//由于文件上传插件暂时无法解决同步提交学校ID，如果是区县或者市级进行上传，则在选择学校时异步将学校ID保存到SESSION中
+			//提交上传文件后,则根据相应权限进行判断学校ID区县ID等信息
+			//然后将上传的文件进度返回到浏览器
+			//并且将校验进度及校验结果返回到浏览器
+
+			//print_r($_REQUEST);
+			//print_r($_FILES);
+			sleep(10);
+			//exit();
+		}
+
 		$this->web_title = '上传学生体质信息';
         $this->page_template = 'Up:phydata';
 	}
